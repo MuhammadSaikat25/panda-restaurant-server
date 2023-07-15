974871b0563ba62fe7f70490bc168fbf0dc2661a
MuhammadSaikat25 <smsaikat000@gmail.com>
2023-07-15 20:04:08 +0600

    added getting all menu section

--- index.js
+++ index.js
@@ -30,8 +30,16 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+    const menu=client.db('panda-restaurant').collection('menu')
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // get all menu
+    app.get('/getMenus',async(req,res)=>{
+        const result=await menu.find().toArray()
+        res.send(result)
+    })
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
