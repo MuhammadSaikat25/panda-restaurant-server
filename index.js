8d4135882c5fee9ee99c785f4e8bf6f0a70afbb5
MuhammadSaikat25 <smsaikat000@gmail.com>
2023-07-17 14:41:37 +0600

    making user in admin

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const express = require('express')
 const app = express()
-const cors = require('cors')
+const cors = require('cors');
 const port = process.env.PORT || 5000
+const jwt=require('jsonwebtoken');
 app.use(cors())
 app.use(express.json())
 require('dotenv').config()
@@ -12,7 +13,21 @@ app.get('/',(req,res)=>{
     res.send('i am so hungry')
 })
 
+const VerifyJwt=(req,res,next)=>{
+    const authorization=req.headers.authorization
+    if(!authorization){
+      return res.status(401).send({error:true,message:'unauthorized access'})
+    }
+    const token=authorization.split(' ')[1]
 
+    jwt.verify(token,process.env.VITE_JWT,(error,decode)=>{
+      if(error){
+        return res.status(401).send({error:true,message:'unauthorized access'})
+      }
+      req.decode=decode
+      next()
+    })
+}
 
 const uri = `mongodb+srv://${process.env.VITE_USER}:${process.env.VITE_PASS}@cluster0.9rjxxtl.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -37,11 +52,27 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
+    // JWT
+    app.post('/jwt',async(req,res)=>{
+      const user=req.body
+      const token= jwt.sign(user,process.env.VITE_JWT,{expiresIn:'1d'})
+      res.send(token)
+    })
+
+    
     // get all menu
     app.get('/getMenus',async(req,res)=>{
         const result=await menu.find().toArray()
         res.send(result)
     })
+    // get single user order
+    app.get('/getSingleUserOrder/:email',VerifyJwt, async(req,res)=>{
+      const email=req.params.email 
+      const query={email:email}
+      const result=await order.find(query).toArray()
+      res.send(result)
+    })
+
     // added user in mongodb
     app.post('/postUser',async(req,res)=>{
       const data=req.body
@@ -56,6 +87,24 @@ async function run() {
       res.send(result)
     })
 
+    // get all user 
+    app.get('/getAllUser',VerifyJwt, async(req,res)=>{
+      const result=await user.find().toArray()
+      res.send(result)
+    })
+
+    //make admin
+    app.patch('/makeAdmin/:email',VerifyJwt, async(req,res)=>{
+      const email=req.params.email
+      const query={email:email}
+      const update={
+        $set:{
+          role:'admin'
+        }
+      }
+      const result =await user.updateOne(query,update)
+      res.send(result)
+    })
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
